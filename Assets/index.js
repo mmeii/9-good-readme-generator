69ef742652f9e6ee88f7a9b06dde40f6236c3c9a
Mengmei Tu <redmmeii@gmail.com>
2020-12-12 17:53:49 -0600

    update index.js using template given

--- Assets/index.js
+++ Assets/index.js
@@ -1,11 +1,76 @@
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is your project title?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your project?',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter instructions for installation',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Is there any contribution you would like to add?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Any test to include?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project?',
+        choices: ["MIT", "Microsoft Public License", "ISC", "GNU GPLv3"]
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+// function writeToFile('README.md', data) {
+// }
 
 // function to initialize program
 function init() {
@@ -14,3 +79,20 @@ function init() {
 
 // function call to initialize program
 init();
+
+// const writeFileAsync = util.promisify(fs.writeFile);
+
+// const promptUser = () =>
+//     inquirer.prompt([
+
+//     ]);
+
+// const generateReadme = (responses) =>
+//     `
+//     `;
+
+// promptUser()
+//     .then((responses) => writeFileAsync('README.md',
+//         generateReadme(responses)))
+//     .then(() => console.log('Successfully wrote to README.md'))
+//     .catch((err) => console.error(err));
--- Assets/template.md
+++ Assets/template.md
@@ -15,5 +15,3 @@ Description
 ## Tests
 
 ## Questions
-
-
