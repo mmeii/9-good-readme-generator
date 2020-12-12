75b02e322a319a8344aeee16452eeb1b1bdd95d0
Mengmei Tu <redmmeii@gmail.com>
2020-12-12 17:02:27 -0600

    add prompt questions

--- index.js
+++ index.js
@@ -13,3 +13,59 @@
 // THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
 // WHEN I click on the links in the Table of Contents
 // THEN I am taken to the corresponding section of the README
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const promptUser = () =>
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is your project title?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'What is the description of your project?',
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter instructions for installation',
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What is the usage information?',
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Is there any contribution you would like to add?',
+        },
+        {
+            type: 'input',
+            name: 'test',
+            message: 'Any test to include?',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your project?',
+            choices: ["MIT", "Microsoft Public License", "ISC", "GNU GPLv3"]
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?',
+        },
+    ]);
